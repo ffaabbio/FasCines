5ffff6ea3fc5d336ef01a1763f381b10ea938f37
Fabio <f.dugay@vocalcom.com>
2026-05-07 14:37:51 +0200

    fix internal error

--- src/database/database.ts
+++ src/database/database.ts
@@ -1,4 +1,11 @@
 import { DataSource } from "typeorm"
+import { User } from "./entities/user.js"
+import { Token } from "./entities/token.js"
+import { Room } from "./entities/room.js"
+import { Movie } from "./entities/movie.js"
+import { Screening } from "./entities/screening.js"
+import { Ticket } from "./entities/ticket.js"
+import { Transaction } from "./entities/transaction.js"
 
 export const AppDataSource = new DataSource({
     type: "postgres",
@@ -7,10 +14,7 @@ export const AppDataSource = new DataSource({
     username: process.env.DB_USER ?? "fascine_user",
     password: process.env.DB_PASSWORD ?? "fascine_password",
     database: process.env.DB_NAME ?? "fascine_db",
-    synchronize: process.env.NODE_ENV !== "production",
+    synchronize: true,
     logging: process.env.NODE_ENV === "development",
-    entities: [process.env.NODE_ENV === "production"
-        ? "dist/database/entities/*.js"
-        : "src/database/entities/*.ts"
-    ],
+    entities: [User, Token, Room, Movie, Screening, Ticket, Transaction],
 })
